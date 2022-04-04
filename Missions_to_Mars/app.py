f6d94d3923bc32f4c9982edd9d2108d533cd52ab
abotsi1080 <95152020+abotsi1080@users.noreply.github.com>
2022-04-04 13:07:07 -0400

    minor change in app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -8,26 +8,26 @@ import scrape_mars
 app = Flask(__name__)
 
 # Using PyMongo to establish Mongo connection
-mongo = PyMongo(app, url="mongodb://localhost:27017/mars_app")
-
+#mongo = PyMongo(app, url="mongodb://localhost:27017/mars_app")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
+mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    mars_dict = mongo.db.mars_dict.find_one()
+    mars_dict = mongo.db.mars_dicts.find_one()
     # Return template and data
     return render_template("index.html", mars_dict=mars_dict)
 
 
 @app.route("/scrape")
-def scrape():
-  
-    mars_dict = mongo.db.mars_dict
+def scraper():
+    mars_dicts = mongo.db.mars_dicts
     mars_data = scrape_mars.scrape()
     # Update the Mongo database using update and upsert=True
-    mars_dict.update({}, mars_data, upsert=True)
+    mars_dicts.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
